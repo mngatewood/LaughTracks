0acddd398208c3253434b6c09ce0941be1fa2343
Michael Gatewood <michael@mngatewood.com>
2018-10-02 18:51:59 -0600

    Adds all comedians route to app

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,11 @@
+require_relative '../models/comedian.rb'
+require_relative '../models/special.rb'
+
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    @comedians = Comedian.all
+    erb :index
+  end
+
 end
