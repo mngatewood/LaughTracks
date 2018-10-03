528cfb80a0c996bd317461b3433df6917a2b2276
Michael Gatewood <michael@mngatewood.com>
2018-10-03 17:29:07 -0600

    Adds tests to special_spec

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -10,11 +10,18 @@ RSpec.describe Special do
     end
 
     describe 'Required Field(s)' do
-      it 'should be invalid if missing a name' do
-        special = Special.create
+      it 'should be invalid if missing a title' do
+        special = Special.create(runtime: 60, comedian_id: 1)
 
         expect(special).to_not be_valid
       end
+
+      it 'should be invalid if missing a comedian_id' do
+        special = Special.create(title: 'A Funny Show', runtime: '60')
+
+        expect(special).to_not be_valid
+      end
+
     end
   end
 end
