cf2c14c262d0372d82f527343d278d05e88eff32
Michael Gatewood <michael@mngatewood.com>
2018-10-04 17:44:57 -0600

    Updates average_age and validation to change birthdate to age

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -2,13 +2,12 @@ require 'pry'
 
 class Comedian < ActiveRecord::Base
 
-  validates_presence_of :name, :birthdate, :city
+  validates_presence_of :name, :age, :city
 
   has_many :specials
 
   def self.average_age
-    sum_of_ages = Comedian.all.sum{|c|(Time.now - c.birthdate.to_time) / 1.year.seconds}
-    return (sum_of_ages / Comedian.all.length).floor
+    Comedian.average(:age)
   end
 
   def self.unique_cities
