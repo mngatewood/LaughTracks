23a2b0a0d02242bfadfbe1bc47ecf8b10c1cc55f
Michael Gatewood <michael@mngatewood.com>
2018-10-05 23:02:19 -0600

    Adds sorting route

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -7,19 +7,21 @@ class LaughTracksApp < Sinatra::Base
   get '/comedians' do
     if params[:age]
       @comedians = Comedian.where(age: params[:age])
-      @average_age = @comedians.average_age
       # average_runtime does not use ActiveRecord
       all_runtimes = @comedians.map{|c|c.specials.map{|s|s.runtime}}.flatten
       @average_runtime = all_runtimes.length > 0 ? all_runtimes.sum / all_runtimes.count : 0
       @unique_cities = @comedians.unique_cities
     else
-      @comedians = Comedian.all
-      @average_age = Comedian.average_age
+      if params[:sort]
+        @comedians = Comedian.order(params[:sort])
+      else
+        @comedians = Comedian.all
+      end
       @average_runtime = Special.average_runtime
       @unique_cities = Comedian.unique_cities
     end
+    @average_age = @comedians.average_age
     @specials_count = @comedians.map{|c|c.specials.count}.sum
-    @today = Date.today
     erb :index
   end
 
